ee340175e036f9323ad815d39632ba3b5b175fcc
bensimonschelhaas <98400638+bensimonschelhaas@users.noreply.github.com>
2024-12-10 13:22:14 +0100

    bens scene

--- Assets/BenPrefab/Scripts/Key.cs
+++ Assets/BenPrefab/Scripts/Key.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     public bool isUnlocked;
+    public AudioSource keyget;
     
     private void OnCollisionEnter(Collision collision)
     {
         gameObject.SetActive(false);
+        keyget.Play();
         isUnlocked = true;
     }
 }
